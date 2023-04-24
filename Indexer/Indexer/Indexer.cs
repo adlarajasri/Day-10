54e8c4cd6b36a081c3affeb5b28f60e4e3fef3a9
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-04-24 09:26:54 +0530

    Add files via upload

---
+++ Indexer/Indexer/Indexer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace IndexerDemo
+{
+        public class indexer
+        {
+                float[] arr = new float[] { 12, 5f, 34, 3f, 5.2f, 6.1f, 7.5f, 88.8f, 22.9f };
+
+                public float this[int index]
+                {
+                    get
+                    {
+                        return arr[index];
+                    }
+                    set
+                    {
+                        arr[index] = value;
+                    }
+                }
+            
+        }
+}
+
